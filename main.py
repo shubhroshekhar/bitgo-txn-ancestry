1f06c6fc432f436bc87e7269d4a72178af35d856
Shubhro Shekhar <shubhroshekhar@gmail.com>
2022-07-25 19:34:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,80 @@
 import requests
+from operator import itemgetter
 
-# UTILS
 class TransactionAncestrySets:
-    def __init__(self, height, max_txn_count=10):
+    """Transaction Ancestry base class
+    Load class with block height 
+    """
+    def __init__(self, height):
         self.height = height
-        self.max_txn_count = max_txn_count
-        self.block_hash = self.get_hash_from_block_heoght()
-        self.transactions = self.get_txns_from_hash()
+        self.block_hash = self.__get_hash_from_block_heoght()
+        self.transactions = self.__get_txns_from_hash()
+        self.__updated_transactions()
     
-    def get_hash_from_block_heoght(self):
+    def __get_hash_from_block_heoght(self):
+        """get hash from given block height 
+
+        Returns:
+            str: block hash
+        """
         rqst = requests.get(f'https://blockstream.info/api/block-height/{self.height}')
-        print(rqst.text)
         return rqst.text
 
-    def get_txns_from_hash(self):
+    def __get_txns_from_hash(self):
+        """gets transaction from hash
+        Returns:
+            dict: transaction details 
+        """
         rqst = requests.get(f'https://blockstream.info/api/block/{self.block_hash}/txs')
-        print(rqst.text)
         return rqst.json()
 
-    def get_linked_transactions_from_transaction(self, txn_id):
+    def get_transactions_detail_from_transaction_id(self, txn_id):
+        """loads transaction details from transaction id
+
+        Args:
+            txn_id (str): transaction_id
+
+        Returns:
+            dict: transaction detals
+        """
         rqst = requests.get(f'https://blockstream.info/api/tx/{txn_id}')
-        print(rqst.text)
-        import pdb; pdb.set_trace()
-        return rqst.json()
+        print(f'getting txn details - {txn_id}')
+        txn_detail = rqst.json()
+        return txn_detail
+
+    def __updated_transactions(self):
+        """loads details of every transaction 
+        """
+        for x in self.transactions:
+            if x['status']['block_hash'] == self.block_hash:
+                trahsaction_details = self.get_transactions_detail_from_transaction_id(x['txid'])
+                x['txn_detail_data'] = trahsaction_details
+                # Assuming vin is the ancestry history list of the transaction 
+                x['count'] = len(trahsaction_details['vin'])
+            else:
+                x['txn_detail_data'] = None
+                x['count'] = 0
+        self.transactions = sorted(self.transactions, key=itemgetter('count'), reverse=True)
+    
+    def get_top_txns(self, max_txn_count):
+        """get top transactions
+
+        Args:
+            max_txn_count (int): count of transaction
+
+        Returns:
+            list: top transaction with max ancestry
+        """
+        n_l = []
+        c = max_txn_count if len(self.transactions) >= max_txn_count else  len(self.transactions)
+        for x in range(c):
+            n_l.append({"txid" : self.transactions[x]["txid"], "ancestrySetSize": self.transactions[x]['count']})
+        return n_l
 
 # MAIN FLOW
 def main():
-    transaction_ancestry_sets = TransactionAncestrySets('680000', 10)
-    print(transaction_ancestry_sets.transactions)
-
-
-
-
+    transaction_ancestry_sets = TransactionAncestrySets('680000')
+    print(transaction_ancestry_sets.get_top_txns(100))
 
 main()
+
